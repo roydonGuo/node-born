bf762f5bb4f870a52ef0118cd772588400153e52
roydon <73972404+roydonGuo@users.noreply.github.com>
2022-09-14 14:41:00 +0800

    Update wallhaven.js

--- wallhaven.js
+++ wallhaven.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const request = require('request')
 const fs = require('fs')
 
-let url = 'https://example/$page='
+let url = 'https://example/img?search$page='
 let page = 23;
 //图片保存路径
 let photoSavePath = './img';
@@ -15,7 +15,6 @@ run(url, page, photoSavePath)
     });
 
 async function run(url, page, photoSavePath) {
-    //  获得首页
     let html;
     for (let i = page; i <= page; i++) {
         html = await getHTML(url + i);
@@ -28,7 +27,6 @@ async function run(url, page, photoSavePath) {
             console.log('toFullImgUrl------' + toFullImgUrl);
             getDetailMessage(toFullImgUrl)
                 .then(imgUrl => {
-                    // return addToFile(movie);
                     console.log(imgUrl);
                     downloadPhoto(imgUrl,photoSavePath)
                 })
@@ -49,27 +47,29 @@ function getHTML(url, timeout = 50000) {
         })
     })
 }
-
-// 添加到文件
-function addToFile(item) {
-    if (item) {
-        let imgSrc = item.img;
-        item = JSON.stringify(item) + '\n';
-        // 把movie 写入文件
-        fs.appendFile('./data/data.txt', item, {
-            encoding: 'utf-8',
-            flag: 'a'
-        }, error => {
-            error ? console.log('Method: addToFile => data write error.\n' + error) : null;
-        })
-        return imgSrc;
+   // 获得图片的full的详细信息
+async function getDetailMessage(url) {
+    // 此处逻辑需要自己定义
+    try {
+        let detailHTML = await getHTML(url);
+        // console.log(detailHTML);
+        var index = detailHTML.indexOf('https://example.com/img/')
+        console.log(index);
+        let fullImgUrl = detailHTML.substring(index, index + 51)
+        // console.log(fullImgUrl);
+        return index > 0 ? fullImgUrl : getDetailMessage(url)
+    } catch (e) {
+        console.log('Error: Message =>' + e);
     }
 }
+
+
 
 
 // 下载图片
 function downloadPhoto(src, photoSavePath, timeout = 20000) {
     if (src) {
+        // 保存路径
         let path = photoSavePath + '/' + src.toString().slice(41);
         // 下载
         request.get(src, {
