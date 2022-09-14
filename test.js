6714bf302825c32196c90b247c63768e566bb787
roydon <73972404+roydonGuo@users.noreply.github.com>
2022-09-14 14:31:50 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const request = require('request')
 const fs = require('fs')
 
-var url = 'https://wallhaven.cc/w/r29w2m'
+var url = 'https://example/'
 var photoSavePath = './imgTop';
 
 run(url,  photoSavePath)
@@ -19,21 +19,10 @@ async function run(url, photoSavePath) {
     var index = html.indexOf('https://w.wallhaven.cc/full/')
     console.log(index);
     console.log(html.substring(index,index+51));
-    // 可以直接用正则进行内容匹配
-    // 把首页 用cheerio模块转换成Dom操作 （注意：load转换时可能会乱码，所以要设置{decodeEntities: false}）
-    // const $ = cheerio.load(html, {
-    //     decodeEntities: false
-    // });
-
-    // let imgUrl = await $('#main #showcase .scrollbox #wallpaper').attr('src')
-    // console.log(imgUrl)
-
 
 }
 
-
-
-// 请求资源
+// 请求html
 function getHTML(url, timeout = 20000) {
     return new Promise((resolve, reject) => {
         // 1.发送请求，等待网站回应
