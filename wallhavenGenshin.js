901f24abe9548f4a777b73a524cba9efc63bc2f7
roydon <73972404+roydonGuo@users.noreply.github.com>
2022-09-14 14:35:46 +0800

    Update wallhavenGenshin.js

--- wallhavenGenshin.js
+++ wallhavenGenshin.js
@@ -1,11 +1,8 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const request = require('request')
-const fs = require('fs')
 
-// https://wallhaven.cc/search?q=id%3A95047&page=
-// https://wallhaven.cc/search?q=genshin&categories=110&purity=100&sorting=favorites&order=desc&page=6
-let url = 'https://wallhaven.cc/search?q=genshin&categories=110&purity=100&sorting=favorites&order=desc&page='
+let url = 'https://example.com&page='
 let page = 40;
 let photoSavePath = './Genshin';
 
@@ -21,12 +18,9 @@ async function run(url, page, photoSavePath) {
     for (i = page; i <= page; i++) {
         html = await getHTML(url + i);
 
-        // 可以直接用正则进行内容匹配
-        // 把首页 用cheerio模块转换成Dom操作 （注意：load转换时可能会乱码，所以要设置{decodeEntities: false}）
         const $ = cheerio.load(html, {
             decodeEntities: false
         });
-        // 2.对返回来的数据进行处理
 
         $('.thumb-listing-page ul li figure .preview').each((i, item) => {
             let toFullImgUrl = $(item).attr('href')
@@ -45,7 +39,6 @@ async function run(url, page, photoSavePath) {
     }
 
 }
-
 // 请求资源
 function getHTML(url, timeout = 50000) {
     return new Promise((resolve, reject) => {
@@ -57,22 +50,6 @@ function getHTML(url, timeout = 50000) {
         })
     })
 }
-
-// 获得图片的full的详细信息
-async function getDetailMessage(url) {
-    try {
-        let detailHTML = await getHTML(url);
-        // console.log(detailHTML);
-        var index = detailHTML.indexOf('https://w.wallhaven.cc/full/')
-        console.log(index);
-        let fullImgUrl = detailHTML.substring(index, index + 51)
-        // console.log(fullImgUrl);
-        return index > 0 ? fullImgUrl : getDetailMessage(url)
-    } catch (e) {
-        console.log('Method: getDetailMessage =>' + e);
-    }
-}
-
 
 // 下载图片
 function downloadPhoto(src, photoSavePath, timeout = 20000) {
